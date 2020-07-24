61c9081f527ae05b37120549de389ce9e20f2b1e
James White <YOUR_EMAIL@DOMAIN.COMjames.ewhitejr@yahoo.com>
2020-07-23 20:49:39 -0400

    added form/ post request

--- frontend/client/src/components/Routes.jsx
+++ frontend/client/src/components/Routes.jsx
@@ -3,6 +3,7 @@ import { Router, Switch, Route, withRouter } from "react-router-dom";
 import Home from "./Home";
 import history from './history';
 import Login from './Login'
+import AddProfile from './AddProfile'
 
 export default class Routes extends Component {
     render() {
@@ -10,6 +11,7 @@ export default class Routes extends Component {
                 <Switch>
                     <Route exact path="/" component={Login} />
                     <Route  exact path="/Home" component={Home} />
+                    <Route exact path="/AddProfile" component={AddProfile}/>
                 </Switch>
         )
     }
--- frontend/client/src/components/form.jsx
+++ frontend/client/src/components/form.jsx
@@ -1,1 +1,18 @@
-import react
+import React from 'react'
+
+const form = (props) => {
+	return(
+		<form className='form' onSubmit ={props.onSubmit}>
+			<input name='image' value={props.img} placeholder='image' onChange={props.onChange}/>
+			<input name='name' value={props.name} placeholder='name' onChange={props.onChange}/>
+			<input name='email' value={props.email} placeholder='email' onChange={props.onChange}/>
+            <input name='name' value={props.name} placeholder='name' onChange={props.onChange}/>
+            <input name='license' value={props.license} placeholder='license' onChange={props.onChange}/>
+            <input name='city' value={props.city} placeholder='city' onChange={props.onChange}/>
+            <input name='state' value={props.state} placeholder='state' onChange={props.onChange}/>
+            <input name='zipcode' value={props.zipcode} placeholder='zipcode' onChange={props.onChange}/>
+            <input name='phone' value={props.phone} placeholder='phone' onChange={props.onChange}/>
+        </form>
+	)
+}
+export default form;
